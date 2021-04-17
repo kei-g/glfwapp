ed7dc77253aff4f43fa65533c1429cf7e13500d5
kei-g <km.8k6ce+github@gmail.com>
2021-04-17 20:06:48 +0900

    :heavy_minus_sign: Remove unnecessary dependencies

--- GLtexture.cpp
+++ GLtexture.cpp
@@ -1,5 +1,15 @@
 #include "GLtexture.h"
 
+#define STBI_ASSERT(expr)
+#define STBI_NO_BMP
+#define STBI_NO_FAILURE_STRINGS
+#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+#define STBI_NO_PSD
+#define STBI_NO_STDIO
+#define STBI_NO_TGA
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
